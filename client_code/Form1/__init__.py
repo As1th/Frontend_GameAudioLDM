cd4f546fd164ae2416e73cc97aa30a55bd6dea51
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-01-29 18:52:39 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,16 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def generate_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Call the google colab function and pass it the iris measurements
+    iris_category = anvil.server.call('predict_iris', 
+                                self.sepal_length.text,
+                                self.sepal_width.text,
+                                self.petal_length.text,
+                                self.petal_width.text)
+    # If a category is returned set our species 
+    if iris_category:
+      self.species_label.visible = True
+      self.species_label.text = "The species is " + iris_category.capitalize()
