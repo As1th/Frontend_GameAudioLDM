dcf587fd9177e7de10eb08254675d7fec347b325
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-01-30 06:26:42 +0000

    Edited standard-page.html and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,5 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,7 +7,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
+  def player_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    src_url = anvil.server.call('load_url')
+    self.audio_player.call_js('loadClip', src_url)
   def generate_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     # Call the google colab function and pass it the iris measurements
@@ -21,3 +23,11 @@ class Form1(Form1Template):
     if iris_category:
       self.species_label.visible = True
       self.species_label.text = "The species is " + iris_category.capitalize()
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def button_1_click(self, **event_args):
+    self.call_js('PlaySound')
+
