24c15755efc77550fa114e4b3d7632f489fb39ce
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-01-30 08:05:33 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.server
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -14,15 +14,13 @@ class Form1(Form1Template):
   def generate_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     # Call the google colab function and pass it the iris measurements
-    iris_category = anvil.server.call('predict_iris', 
-                                self.sepal_length.text,
-                                self.sepal_width.text,
-                                self.petal_length.text,
-                                self.petal_width.text)
+    iris_category = anvil.server.call('test', 
+                                self.prompt.text
+                                )
     # If a category is returned set our species 
     if iris_category:
-      self.species_label.visible = True
-      self.species_label.text = "The species is " + iris_category.capitalize()
+     
+      self.negativePrompt.text = "The species is " + iris_category
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
