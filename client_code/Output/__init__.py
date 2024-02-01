e17e653d03409f81fb3962716504408452721555
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-02-01 19:31:09 +0000

    Edited Output

--- client_code/Output/__init__.py
+++ client_code/Output/__init__.py
@@ -21,9 +21,29 @@ class Output(OutputTemplate):
     self.call_js('PlayAudio1')
 
   def CheckAudio1(self, **event_args):
-    if self.audio_check_1.checked:
-       self.audio_play_1.background = 'theme:Primary 500'
+    self.CheckAudio(self.audio_check_1, self.audio_play_1)
+  def CheckAudio2(self, **event_args):
+    self.CheckAudio(self.audio_check_2, self.audio_play_2)
+  def CheckAudio3(self, **event_args):
+    self.CheckAudio(self.audio_check_3, self.audio_play_3)
+  def CheckAudio4(self, **event_args):
+    self.CheckAudio(self.audio_check_4, self.audio_play_4)
+  def CheckAudio5(self, **event_args):
+    self.CheckAudio(self.audio_check_5, self.audio_play_5)
+  def CheckAudio6(self, **event_args):
+    self.CheckAudio(self.audio_check_6, self.audio_play_6)
+  def CheckAudio7(self, **event_args):
+    self.CheckAudio(self.audio_check_7, self.audio_play_7)
+  def CheckAudio8(self, **event_args):
+    self.CheckAudio(self.audio_check_8, self.audio_play_8)
+  def CheckAudio9(self, **event_args):
+    self.CheckAudio(self.audio_check_9, self.audio_play_9)
+
+  def CheckAudio(self, checkbox, player):
+    if checkbox.checked:
+       player.background = 'theme:Primary 500'
     else:
-       self.audio_play_1.background = 'theme:Gray 600'
+       player.background = 'theme:Gray 600'
        
+  
        
