96aeff5d00ae02d5eb01fb04d7d9732f03ecbab6
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-02-01 13:58:05 +0000

    Edited Output

--- client_code/Output/__init__.py
+++ client_code/Output/__init__.py
@@ -3,6 +3,15 @@ from anvil import *
 import anvil.server
 
 class Output(OutputTemplate):
+  Audio1Checked = 0
+  Audio2Checked = 0
+  Audio3Checked = 0
+  Audio4Checked = 0
+  Audio5Checked = 0
+  Audio6Checked = 0
+  Audio7Checked = 0
+  Audio8Checked = 0
+  Audio9Checked = 0
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -10,3 +19,6 @@ class Output(OutputTemplate):
     # Any code you write here will run before the form opens.
   def PlayAudio1(self, **event_args):
     self.call_js('PlayAudio1')
+
+  #def CheckAudio1(self, **event_args):
+    
