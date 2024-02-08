b0c106f617ebf4414a3df538ac8d03882025d975
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-02-08 07:36:01 +0000

    Edited standard-page.html and Input

--- client_code/Input/__init__.py
+++ client_code/Input/__init__.py
@@ -1,6 +1,9 @@
 from ._anvil_designer import InputTemplate
 from anvil import *
 import anvil.server
+import base64
+
+
 class Input(InputTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -12,12 +15,18 @@ class Input(InputTemplate):
     
     """This method is called when the button is clicked"""
     # Call the google colab function and pass it the iris measurements
-    audio = anvil.server.call('test', 
+    audio_base64 = anvil.server.call('test', 
                                 self.prompt.text, self.negative_prompt.text, 1
                                 )
     # If a category is returned set our species 
-    if audio:
-      self.negative_prompt.text = "The species is " + audio
+    if audio_base64:
+      self.negative_prompt.text = "The species is " + audio_base64
+    audio_bytes = base64.b64decode(audio_base64)
+
+    #
+    audio_base64 = 
+    #
+    self.call_js('PlayAudioFromBytes',audio_base64)
 
     #open_form('Output')
 
