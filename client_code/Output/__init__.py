ef58aa077a76c6ce797c7b36e392a67bf80082ca
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-02-08 13:14:10 +0000

    Edited standard-page.html, Input and Output

--- client_code/Output/__init__.py
+++ client_code/Output/__init__.py
@@ -12,6 +12,8 @@ class Output(OutputTemplate):
   Audio7Checked = 0
   Audio8Checked = 0
   Audio9Checked = 0
+  
+
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
