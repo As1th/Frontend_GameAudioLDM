cbbbad2fd85dfda87960e809115a7bca981c0aa1
asith.20200853@iit.ac.lk <asith.20200853@iit.ac.lk>
2024-02-11 18:39:36 +0000

    Edited Input

--- client_code/Input/__init__.py
+++ client_code/Input/__init__.py
@@ -13,8 +13,7 @@ class Input(InputTemplate):
     self.init_components(**properties)
   
   def generate_button_click(self, **event_args):
-    open_form('Output', gens= self.number_of_gens.selected_value)
-    # Call the google colab function and pass it the iris measurements
+    # Call the google colab function and pass it the parameters
     audio_base64_list = anvil.server.call('GenerateAudio', 
                                 self.prompt.text, self.negative_prompt.text, int(self.number_of_gens.selected_value)
                                 )
